29fc4519003f92ceeb41e873b4bec03de1716bf0
Athos Chagas <athosbhx@gmail.com>
2023-06-11 17:01:52 -0300

    first commit

---
+++ questions.js
@@ -0,0 +1,34 @@
+export default [
+    {
+question: "Quanto é 10 + 25?",
+answers: [
+        { option: "35", correct: true },
+        { option: "45", correct: false },
+        { option: "25", correct: false },
+],
+    },
+    {
+question: "Quanto é 5 x 7?",
+answers: [
+        { option: "49", correct: false },
+        { option: "35", correct: true },
+        { option: "32", correct: false },
+],
+    },
+    {
+question: "Quanto é 11 x 8?",
+answers: [
+        { option: "880", correct: false },
+        { option: "90", correct: false },
+        { option: "88", correct: true },
+],
+    },
+    {
+question: "Quanto é 7 x 7?",
+answers: [
+        { option: "70", correct: false },
+        { option: "56", correct: false },
+        { option: "49", correct: true },
+],
+    },
+];
